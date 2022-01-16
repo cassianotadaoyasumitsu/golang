db4a1925f2b7d092d5b12fbcaeceb615bc12b5ce
cassiano yasumitsu <cassiano@usuarioincomum.com>
2022-01-16 19:14:32 +0900

    maps

---
+++ Maps/maps.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Maps")
+
+	usuario := map[string]string{
+		"nome":      "Pedro",
+		"sobrenome": "Silva",
+	}
+	fmt.Println(usuario["nome"])
+
+	usuario2 := map[string]map[string]string{
+		"nome": {
+			"primeiro": "João",
+			"ultimo":   "Pedro",
+		},
+		"curso": {
+			"nome":   "Eng.",
+			"campus": "Campus 1",
+		},
+	}
+	fmt.Println(usuario2)
+	delete(usuario2, "nome")
+	fmt.Println(usuario2)
+
+	usuario2["signo"] = map[string]string{
+		"nome": "Sagitário",
+	}
+	fmt.Println(usuario2)
+}
