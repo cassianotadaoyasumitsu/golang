3b3e412fcf821a9de65d7a38d8d3b767a791dfae
cassiano yasumitsu <cassiano@usuarioincomum.com>
2022-01-16 19:00:55 +0900

    slice by make

--- Arrays e Slices/arrays-e-slices.go
+++ Arrays e Slices/arrays-e-slices.go
@@ -30,4 +30,22 @@ func main() {
 	fmt.Println(slice2)
 	array2[1] = "Position diff"
 	fmt.Println(slice2)
+
+	// Arrays Internos
+
+	slice3 := make([]float32, 10, 11)
+	fmt.Println(slice3)
+
+	slice3 = append(slice3, 5)
+	slice3 = append(slice3, 6)
+	fmt.Println(slice3)
+	fmt.Println(len(slice3)) // length
+	fmt.Println(cap(slice3)) // capacidade
+
+	slice4 := make([]float32, 5)
+	slice4 = append(slice4, 10)
+	fmt.Println(slice4)
+	fmt.Println(len(slice4)) // length
+	fmt.Println(cap(slice4)) // capacidade
+
 }
