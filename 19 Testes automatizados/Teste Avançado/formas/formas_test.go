2d1882ed289fbbbda7cf6d8671ff7b8cc6144364
cassiano yasumitsu <cassiano@usuarioincomum.com>
2022-02-06 18:10:28 +0900

    TDD

---
+++ 19 Testes automatizados/Teste Avançado/formas/formas_test.go
@@ -0,0 +1,31 @@
+package formas
+
+import (
+	"math"
+	"testing"
+)
+
+func TestArea(t *testing.T) {
+	// TDD - Test Driven Development
+
+	t.Run("Retangulo", func(t *testing.T) {
+		ret := Retangulo{10, 12}
+		areaEsperada := float64(120)
+		areaRecebida := ret.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Fatalf("A área recebida %f é diferente da esperada %f", areaRecebida, areaEsperada)
+		}
+	})
+
+	t.Run("Circulo", func(t *testing.T) {
+		circ := Circulo{10}
+
+		areaEsperada := float64(math.Pi * 100)
+		areaRecebida := circ.Area()
+
+		if areaEsperada != areaRecebida {
+			t.Fatalf("A área recebida %f é diferente da esperada %f", areaRecebida, areaEsperada)
+		}
+	})
+}
